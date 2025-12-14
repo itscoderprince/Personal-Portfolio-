a9550feaf3d043246671c43c4bd47f0dc3f8a2c9
itscoderprince <itscoderprince@gmail.com>
2025-12-14 12:17:05 +0530

    Force fix: Vercel preset and config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here - triggered rebuild */
+  /* Vercel Framework Preset Fix */
   reactCompiler: true,
 };
 
